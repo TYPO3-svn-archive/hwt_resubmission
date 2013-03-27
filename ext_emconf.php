1d0e290c84b8b382e9385d5e13e811b96fc88a9a
hwt3 <hwt3@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-27 16:14:08 +0000

    2013-03-25 Heiko Westermann  <hwt3@gmx.de> * initial release v 0.0.1 experimental * feature: service to check for open resubmissions of current user in 'tstamp' of 'tt_content' records * feature: hook for displaying backendwarning if necessary * feature: option to configure time for resubmit in em
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/hwt_resubmission/tags/0.0.1@73832 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "hwt_resubmission".
+#
+# Auto generated 25-03-2013 14:58
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'hwt resubmission',
+	'description' => 'Mange resubmissions for be users',
+	'category' => 'module',
+	'author' => 'Heiko Westermann',
+	'author_email' => 'hwt3@gmx.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'experimental',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"d372";s:21:"ext_conf_template.txt";s:4:"244e";s:12:"ext_icon.gif";s:4:"1491";s:14:"ext_tables.php";s:4:"e71f";s:10:"README.txt";s:4:"ee2d";s:13:"mod1/conf.php";s:4:"61eb";s:14:"mod1/index.php";s:4:"ddee";s:18:"mod1/locallang.xml";s:4:"7039";s:22:"mod1/locallang_mod.xml";s:4:"4254";s:22:"mod1/mod_template.html";s:4:"7c59";s:19:"mod1/moduleicon.gif";s:4:"032b";}',
+	'suggests' => array(
+	),
+);
+
+?>
---
+++ hooks/class.tx_hwtresubmission_backendwarnings.php
@@ -0,0 +1,66 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2013 Heiko Westermann <hwt3@gmx.de>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ * $Id$
+ */
+
+require_once(t3lib_extMgm::extPath('hwt_resubmission', 'sv1/class.tx_hwtresubmission_sv1.php'));
+
+/**
+ * This class contains a hook to the backend warnings collection. It checks
+ * for open resubmissions and creates a warning if the configuration is wrong.
+ *
+ * @author	Heiko Westermann <hwt3@gmx.de>
+ * @package	TYPO3
+ * @subpackage	tx_hwtresubmission
+ */
+class tx_hwtresubmission_backendwarnings {
+
+	/**
+	 * Checks for open resubmissions and creates warnings if necessary.
+	 *
+	 * @param	array	$warnings	Warnings
+	 * @see	t3lib_BEfunc::displayWarningMessages()
+	 */
+	public function displayWarningMessages_postProcess(array &$warnings) {
+        $extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['hwt_resubmission']);
+        $openResubmissions = tx_hwtresubmission_sv1::getOpenResubmissions($GLOBALS[$BE_USER]->user['uid'], $extConf['resubmissionTime']);
+
+        if($openResubmissions) {
+            $warnings['hwtresubmission'] = '[EXT: hwt_resubmission] Open Resubmissions:<br />';
+            foreach($openResubmissions as $value) {
+                $warnings['hwtresubmission'] .= '- tt_content[' . $value['uid'] . '] ' . $value['header'] . '<br />';
+            }
+            $warnings['hwtresubmission'] .= '<a href="javascript:top.goToModule(\'web_list\',1)">zum List-Modul</a>';
+        }
+	}
+}
+
+if (defined('TYPO3_MODE') && isset($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/hwt_resubmission/hooks/class.tx_hwtresubmission_backendwarnings.php'])) {
+	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/hwt_resubmission/hooks/class.tx_hwtresubmission_backendwarnings.php']);
+}
+?>
